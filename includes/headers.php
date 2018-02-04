2d8edab43083cb17ea0ce7d7e995095bd280885d
Paul Gonzaga <32408458+plgnzaga@users.noreply.github.com>
2018-02-04 23:43:22 +0800

    Update headers.php

--- includes/headers.php
+++ includes/headers.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="css/mystyle.css">
 <link rel="stylesheet" href="css/bootstrap.min.css">
-<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
 <script type="text/javascript" src="js/thescript.js"></script>
